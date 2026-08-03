411eb1d590114201688e112909b56e9f6dd23b00
alexgrosse98-spec <alexgrosse98@gmail.com>
2026-08-03 13:50:08 +0200

    Add files via upload

--- service-worker.js
+++ service-worker.js
@@ -1,5 +1,5 @@
-const CACHE='fahrplan-a1a2-v8';
-const FILES=['./','./index.html?v=8','./data.json?v=8','./manifest.webmanifest?v=8','./icon.svg'];
+const CACHE='busfahrplan-monopoli-capitolo-v9';
+const FILES=['./','./index.html?v=9','./data.json?v=9','./manifest.webmanifest?v=9','./icon.svg'];
 self.addEventListener('install',e=>{self.skipWaiting();e.waitUntil(caches.open(CACHE).then(c=>c.addAll(FILES)))});
 self.addEventListener('activate',e=>e.waitUntil(caches.keys().then(k=>Promise.all(k.filter(x=>x!==CACHE).map(x=>caches.delete(x)))).then(()=>self.clients.claim())));
 self.addEventListener('fetch',e=>{if(e.request.method!=='GET')return;e.respondWith(fetch(e.request).then(r=>{const c=r.clone();caches.open(CACHE).then(x=>x.put(e.request,c));return r}).catch(()=>caches.match(e.request)))});
